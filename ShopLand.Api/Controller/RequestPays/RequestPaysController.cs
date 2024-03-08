2347a45f0654a7539bd5790645fb976b1429bb19
mohammadKazem <mmadkzm@gmail.com>
2024-03-08 12:18:00 +0330

    added Finance Api

---
+++ ShopLand.Api/Controller/RequestPays/RequestPaysController.cs
@@ -0,0 +1,40 @@
+using ShopLand.Application.RequestPays.Commands.CreateRequestPay.Request;
+using ShopLand.Application.RequestPays.Facade;
+using ShopLand.Application.RequestPays.Queries.GetRequestPay.Request;
+using ShopLand.Application.RequestPays.Queries.GetRequestPaysUser.Request;
+
+namespace ShopLand.Api.Controller.RequestPays;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class RequestPaysController(IRequestPayFacade requestPayFacade)
+    : ControllerBase
+{
+    private readonly IRequestPayFacade _requestPayFacade = requestPayFacade;
+
+    [HttpPost]
+    public async Task<IActionResult> Post
+        ([FromBody] CreateRequestPayCommandRequest request)
+    {
+        await _requestPayFacade.CreateRequestPay.HandelAsync(request);
+        return Created();
+    }
+
+    [HttpGet("{RequestPayId:guid}")]
+    public async Task<IActionResult> Get
+        ([FromHeader] GetRequestPayQueryRequest request)
+    {
+        var result = await _requestPayFacade.GetRequestPay.HandelAsync(request);
+        return Ok(result);
+    }
+
+    [HttpGet("User/{UserId:guid}")]
+    public async Task<IActionResult> GetByUserId
+        ([FromHeader] GetRequestPaysUserQueryRequest request)
+    {
+        var result = await _requestPayFacade.GetRequestPaysUser
+            .HandelAsync(request);
+        return Ok(result);
+    }
+}
